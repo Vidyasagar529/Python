a0e4f4a0bf605c34a21cac9750651a75882332c4
Vidyasagar <51742423+Vidyasagar529@users.noreply.github.com>
2025-05-31 20:00:26 +0530

    Add files via upload

---
+++ Add.py
@@ -0,0 +1,15 @@
+x = 10
+y = 20
+
+print(f"Sum of {x} and {y} is {x+y}") 
+
+
+# Take input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the two numbers
+sum = num1 + num2
+
+# Display the result
+print("The sum is:", sum)
